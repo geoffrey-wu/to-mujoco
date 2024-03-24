0c5dd0a0252900ee777908605d16924335eff547
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-03-24 21:02:10 +0000

    delete old handem files

--- handem/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-from handem.tasks.ihm_base import IHMBase
-from handem.tasks.handem_classify import HANDEM_Classify
-from handem.tasks.handem_reconstruct import HANDEM_Reconstruct
-
-task_map = {
-    "IHM": IHMBase,
-    "HANDEM_Classify": HANDEM_Classify,
-    "HANDEM_Reconstruct": HANDEM_Reconstruct,
-}
--- scripts/vis_policy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-EXP_NAME=$1
-TASK=$2
-
-python3 train.py task=${TASK} headless=False pipeline=gpu \
-task.env.numEnvs=1 test=True \
-train.algo=HANDEM \
-train.handem.output_name=test_logs/"${EXP_NAME}" \
-checkpoint="${EXP_NAME}"
