851bd2ba074b6435530a43076e40f79dbbaa09f5
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-04-09 02:10:00 +0000

    manually adjust XLA_PYTHON_CLIENT_MEM_FRACTION

--- train.sh
+++ train.sh
@@ -12,5 +12,6 @@ PATH=/usr/local/cuda-11.8/bin:$PATH \
 LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64 \
 CUDA_VISIBLE_DEVICES=$GPU \
 # TF_CPP_MIN_LOG_LEVEL=0 \
-XLA_PYTHON_CLIENT_PREALLOCATE=false \
-python train.py
+XLA_PYTHON_CLIENT_PREALLOCATE=true \
+XLA_PYTHON_CLIENT_MEM_FRACTION=.25 \
+python train.py --env_name $ENV_NAME --experiment_name $EXPERIMENT_NAME
