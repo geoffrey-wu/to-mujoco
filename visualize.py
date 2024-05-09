1be3276669d20d2f75308553bd9b818448126cfe
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-08 20:07:28 -0400

    fix visualization

--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 import argparse
+from tqdm import tqdm
 
 from envs.humanoid import Humanoid
 from envs.ihm_base import IHMBase
@@ -17,6 +18,7 @@ from brax import math
 from brax.base import Base, Motion, Transform
 from brax.envs.base import Env, PipelineEnv, State
 from brax.mjx.base import State as MjxState
+from brax.training.acme import running_statistics
 from brax.training.agents.ppo import train as ppo
 from brax.training.agents.ppo import networks as ppo_networks
 from brax.io import html, mjcf, model
@@ -30,12 +32,19 @@ import mujoco
 from mujoco import mjx
 
 
-def visualize(env_name='humanoid', model_path='models/mjx_brax_policy.mjx', render_path='html_renders/render.html'):
+def visualize(
+    env_name="humanoid",
+    model_path="models/mjx_brax_policy.mjx",
+    render_path="html_renders/render.html",
+):
     env = envs.get_environment(env_name)
 
-    def get_make_inference_fn():
+    def get_make_inference_fn(num_envs=16384):
         ppo_network = ppo_networks.make_ppo_networks(
-            env.observation_size, env.action_size)
+            env.observation_size,
+            env.action_size,
+            preprocess_observations_fn=running_statistics.normalize,
+        )
         make_inference_fn = ppo_networks.make_inference_fn(ppo_network)
         return make_inference_fn
 
@@ -60,32 +69,40 @@ def visualize(env_name='humanoid', model_path='models/mjx_brax_policy.mjx', rend
     n_steps = 500
     render_every = 2
 
-    for i in range(n_steps):
+    for i in tqdm(range(n_steps)):
         act_rng, rng = jax.random.split(rng)
         ctrl, _ = jit_inference_fn(state.obs, act_rng)
         state = jit_step(state, ctrl)
         rollout.append(state.pipeline_state)
 
         if state.done:
+            print(rollout)
             break
 
-    # media.show_video(env.render(rollout[::render_every], camera='side'), fps=1.0 / env.dt / render_every)
+    # media.show_video(
+    #     env.render(rollout[::render_every], camera="side"),
+    #     fps=1.0 / env.dt / render_every,
+    # )
 
-    with open(render_path, 'w') as f:
+    with open(render_path, "w") as f:
         f.write(HTML(html.render(env.sys.replace(dt=env.dt), rollout)).data)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_name', type=str, default='humanoid')
-    parser.add_argument('--experiment_name', type=str, default='experiment')
+    parser.add_argument("--env_name", type=str, default="humanoid")
+    parser.add_argument("--experiment_name", type=str, default="experiment")
 
     args = parser.parse_args()
     env_name = args.env_name
     experiment_name = args.experiment_name
 
-    visualize(env_name, f'models/{experiment_name}.mjx', render_path=f'html_renders/{experiment_name}.html')
+    visualize(
+        env_name,
+        f"models/{experiment_name}.mjx",
+        render_path=f"html_renders/{experiment_name}.html",
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
