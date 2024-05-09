54bfe857b6932f58bf51cc68c341768e75bf2bde
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-08 20:07:48 -0400

    add visualization script

---
+++ visualize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GPU=$1
+ENV_NAME=$2
+EXPERIMENT_NAME=$3
+
+# if any of the arguments are empty
+if [ -z "$GPU" ] || [ -z "$ENV_NAME" ] || [ -z "$EXPERIMENT_NAME" ]; then
+    echo "Usage: $0 <gpu> <env_name> <experiment_name>"
+    exit 1
+fi
+
+MUJOCO_GL=egl \
+PATH=/usr/local/cuda-11.8/bin:$PATH \
+LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64 \
+CUDA_VISIBLE_DEVICES=$GPU \
+# TF_CPP_MIN_LOG_LEVEL=0 \
+# XLA_PYTHON_CLIENT_MEM_FRACTION=.13 \
+python visualize.py --env_name $ENV_NAME --experiment_name $EXPERIMENT_NAME
